1179555a34031f9e02d893c2e28e5ffce9bc476c
Ducau.Lucas <lucas.ducau64@gmail.com>
2023-11-17 16:26:09 +0100

    meh

--- src/app/stats-accueil/stats-accueil.component.ts
+++ src/app/stats-accueil/stats-accueil.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stats-accueil',
   templateUrl: './stats-accueil.component.html',
   styleUrls: ['./stats-accueil.component.css']
 })
-export class StatsAccueilComponent {
+export class StatsAccueilComponent implements OnInit {
+  cards = [
+    { title: 'Card 1', number: 0, target: 100},
+    { title: 'Card 2', number: 0, target: 200},
+    { title: 'Card 3', number: 0, target: 300}
+  ];
 
+
+  constructor() {}
+
+
+  ngOnInit(): void {
+    this.cards.forEach((card, index) => {
+      setInterval(() => {
+        if (card.number < card.target) {
+          card.number++;
+        }
+      }, 1000 / (index + 1));
+    });
+  }
 }
